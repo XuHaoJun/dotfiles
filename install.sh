4966f565f99e7afe331a2dbe16100b880e93f3ad
Xu Hao Jun <XuHaoJunTW@gmail.com>
2012-08-26 14:41:43 +0800

    add dotfile list and rewrite instll.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-cp -u .emacs .pentadactylrc .vimrc .xinitrc .Xmodmap .Xresources .xscreensaver .xsession .zshrc $HOME/
+cp -uv ./{.emacs,.pentadactylrc,.vimrc,.xinitrc,.Xmodmap,.Xresources,.xscreensaver,.xsession,.zshrc,.tmux.conf,.gitconfig} $HOME
 
 ## copy to dir if not have dir then mkdir and copy. 
-cp -u rc.lua $HOME/.config/awesome ||  (mkdir -p $HOME/.config/awesome && cp -u rc.lua $HOME/.config/awesome) 
+cp -uv rc.lua $HOME/.config/awesome ||  (mkdir -p $HOME/.config/awesome && cp -u rc.lua $HOME/.config/awesome) 

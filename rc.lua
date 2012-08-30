1d1e6bcc13e45c06b27f3c05d4a1fdc9f68e52cd
Xu Hao Jun <XuHaoJunTW@gmail.com>
2012-08-31 00:18:07 +0800

    disable awesome wm right text clock

--- rc.lua
+++ rc.lua
@@ -95,7 +95,7 @@ mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
 
 -- {{{ Wibox
 -- Create a textclock widget
-mytextclock = awful.widget.textclock({ align = "right" })
+--mytextclock = awful.widget.textclock({ align = "right" })
 
 -- Create a systray
 mysystray = widget({ type = "systray" })

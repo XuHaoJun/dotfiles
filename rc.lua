5b93c9193a5067db96b9b3c757f98c63ecb0a6cc
Xu Hao Jun <XuHaoJunTW@gmail.com>
2012-08-30 15:58:08 +0800

    correct the gui daemon name form awesome run

--- rc.lua
+++ rc.lua
@@ -413,6 +413,6 @@ awful.util.spawn_with_shell("xcompmgr -Ss -n -Cc -fF -I-10 -O-10 -D1 -t-3 -l-4 -
 awful.util.spawn_with_shell("wicd-client --tray")
 awful.util.spawn_with_shell("dropboxd")
     -- laptop
-awful.util.spawn_with_shell("battarymon")
+awful.util.spawn_with_shell("batterymon")
 -- }}}
 

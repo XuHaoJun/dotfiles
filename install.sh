dd5bd9cce665d6a9875ef5e875f9fdc712903c87
Xu Hao Jun <XuHaoJunTW@gmail.com>
2012-10-10 23:51:31 +0800

    Change -uv to -av

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-cp -uv ./{.emacs,.pentadactylrc,.vimrc,.xinitrc,.Xmodmap,.Xresources,.xscreensaver,.xsession,.zshrc,.tmux.conf,.gitconfig} $HOME
+cp -av ./{.emacs,.pentadactylrc,.vimrc,.xinitrc,.Xmodmap,.Xresources,.xscreensaver,.xsession,.zshrc,.tmux.conf,.gitconfig} $HOME
 
 ## copy to dir if not have dir then mkdir and copy. 
-cp -uv rc.lua $HOME/.config/awesome ||  (mkdir -p $HOME/.config/awesome && cp -u rc.lua $HOME/.config/awesome) 
+cp -av rc.lua $HOME/.config/awesome/ ||  (mkdir -p $HOME/.config/awesome && cp -av rc.lua $HOME/.config/awesome) 

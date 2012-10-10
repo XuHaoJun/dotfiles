a848bd9ba1205f2c73395fb21a5a62ed73fd23e6
Xu Hao Jun <XuHaoJunTW@gmail.com>
2012-10-10 23:53:33 +0800

    Add .xbindkeysrc on the dotfile install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-cp -av ./{.emacs,.pentadactylrc,.vimrc,.xinitrc,.Xmodmap,.Xresources,.xscreensaver,.xsession,.zshrc,.tmux.conf,.gitconfig} $HOME
+cp -av ./{.emacs,.pentadactylrc,.vimrc,.xinitrc,.Xmodmap,.Xresources,.xscreensaver,.xsession,.zshrc,.tmux.conf,.gitconfig,.xbindkeysrc} $HOME
 
 ## copy to dir if not have dir then mkdir and copy. 
 cp -av rc.lua $HOME/.config/awesome/ ||  (mkdir -p $HOME/.config/awesome && cp -av rc.lua $HOME/.config/awesome) 

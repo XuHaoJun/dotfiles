8a739a1793cf998dec8d8faec62e526841b3a306
Xu Hao Jun <XuHaoJunTW@gmail.com>
2013-02-08 09:25:56 +0800

    Add irssi on update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ dir="$HOME/Dropbox/dotfiles"
 
 cp -uv $HOME/{.emacs,.pentadactylrc,.vimrc,.xinitrc,.Xmodmap,.Xresources,.xscreensaver,.xsession,.xbindkeysrc,.zshrc,.tmux.conf,.gitconfig,.gtkrc-2.0} $dir
 cp -uv $HOME/.config/awesome/rc.lua $dir
-
+cp -uv $HOME/.irssi/config $dir/.irssi
 ## default dir is awesome my customize theme
 ## must use sudo cp -ru /usr/share/awesome/themes/default .

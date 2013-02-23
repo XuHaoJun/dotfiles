b7de22deae7d5596db1e2c3c79838413b7cf90f1
Xu Hao Jun <XuHaoJunTW@gmail.com>
2013-02-23 20:45:36 +0800

    Improve install.sh write style.

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/zsh
 
 REPEATED_FILES=$(basename -a .* ${HOME}/.* | sort | uniq -d)
-REPEATED_FILE_COUNT=$(basename -a .* ${HOME}/.* | sort | uniq -d | wc -l)
+REPEATED_FILE_COUNT=$(echo $REPEATED_FILES | wc -l)
 
 if [ "$REPEATED_FILE_COUNT" != 0 ];then
 		echo "----The repeated file list----"
 		echo $REPEATED_FILES
+		echo "total repeated file: $REPEATED_FILE_COUNT"
 		echo "----End of repeated file list----"
 
 		echo "Your HOME dir have repeated file. Do you overwrite it?(y/n)"

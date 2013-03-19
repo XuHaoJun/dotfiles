07bfa50010df40bdbdf69e199b540b64622b6105
XuHaoJun <XuHaoJunTW@gmail.com>
2013-03-19 11:07:23 +0800

    Change indent style.

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@ REPEATED_FILES=$(basename -a .* ${HOME}/.* | sort | uniq -d)
 REPEATED_FILE_COUNT=$(echo $REPEATED_FILES | wc -l)
 
 if [ "$REPEATED_FILE_COUNT" != 0 ];then
-		echo "----The repeated file list----"
-		echo $REPEATED_FILES
-		echo "total repeated file: $REPEATED_FILE_COUNT"
-		echo "----End of repeated file list----"
+    echo "----The repeated file list----"
+    echo $REPEATED_FILES
+    echo "total repeated file: $REPEATED_FILE_COUNT"
+    echo "----End of repeated file list----"
 
-		echo "Your HOME dir have repeated file. Do you overwrite it?(y/n)"
-		read response
-		echo
-		if [[ $response =~ ^[Yy]$ ]]; then
-				rsync --exclude ".git/" --exclude "*~" -av .* $HOME
-		fi
+    echo "Your HOME dir have repeated file. Do you overwrite it?(y/n)"
+    read response
+    echo
+    if [[ $response =~ ^[Yy]$ ]]; then
+	rsync --exclude ".git/" --exclude "*~" -av .* $HOME
+    fi
 fi
 
 exit 0

a16b177c1df3cfc48f886519123447d7a3ba28ce
XuHaoJun <XuHaoJunTW@gmail.com>
2014-04-27 16:33:27 +0800

    Add clojure auto-complete for lein project

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,2 @@
-{:user {:plugins [[cider/cider-nrepl "0.6.0"], [lein-kibit "0.0.8"]] }}
+{:user {:plugins [[cider/cider-nrepl "0.6.0"], [lein-kibit "0.0.8"]] 
+        :dependencies [[compliment "0.0.3"]]}}

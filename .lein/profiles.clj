908ecf93f6c005cb1c9249f434415f03f93dd132
XuHaoJun <XuHaoJunTW@gmail.com>
2014-04-28 13:29:19 +0800

    Add coljurescript dep to lein project.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,2 @@
 {:user {:plugins [[cider/cider-nrepl "0.6.0"], [lein-kibit "0.0.8"]] 
-        :dependencies [[compliment "0.0.3"]]}}
+        :dependencies [[compliment "0.0.3"], [org.clojure/clojurescript "0.0-2202"]]}}

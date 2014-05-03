ad7f49110c46f09280dff7c7d870515bdf1e961c
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-03 13:37:34 +0800

    Add lein plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.6.0"], [lein-kibit "0.0.8"]] 
+{:user {:plugins [[cider/cider-nrepl "0.6.0"]
+                  [lein-kibit "0.0.8"]
+                  [lein-cljsbuild "1.0.3"]
+                  [lein-npm "0.4.0"]
+                  ] 
         :dependencies [[compliment "0.0.3"], [org.clojure/clojurescript "0.0-2202"]]}}
